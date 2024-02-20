b98e7f288429e751d2d938c06752b33a2eb70247
l-v-react <109346622+laszlo-v@users.noreply.github.com>
2024-02-20 16:30:03 +0000

    message min length set

--- modules/email-validation/message.js
+++ modules/email-validation/message.js
@@ -14,6 +14,11 @@ export const messageValidation = (
     messageLabel.classList.add("active");
     messageLabel.textContent = "Message is required!";
     isMessageValid = false;
+  } else if (message.value.trim().length < 50) {
+    message.classList.add("alert");
+    messageLabel.classList.add("active");
+    messageLabel.textContent = "Minimum 50 characters!";
+    isMessageValid = false;
   } else if (hasMessageInteracted && message.value.trim() !== "") {
     message.classList.remove("alert");
     messageLabel.textContent = "Message:";
